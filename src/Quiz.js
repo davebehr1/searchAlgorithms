5d7d5360d7b885a61f1a9ddcf5710503f1e13d6b
davidbehr <david.behr@stackworx.io>
2020-09-29 16:41:10 +0200

    added validation

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,6 +1,12 @@
 import React from "react";
 import classes from "./search.module.css";
+import * as yup from "yup";
 import { Formik, Field, Form } from "formik";
+
+const schema = yup.object().shape({
+  question1: yup.string().required("please answer this question"),
+  question2: yup.string().required("please answer this question"),
+});
 
 export function Quiz() {
   return (
@@ -10,23 +16,25 @@ export function Quiz() {
       <Formik
         initialValues={{
           question1: "",
-          question2: 0,
+          question2: "",
         }}
+        validationSchema={schema}
         onSubmit={async (values) => {
+          console.log("submitting");
           await new Promise((r) => setTimeout(r, 500));
           alert(JSON.stringify(values, null, 2));
         }}
       >
-        {({ values }) => (
-          <Form className={classes.quiz}>
+        {({ handleSubmit }) => (
+          <Form className={classes.quiz} onSubmit={handleSubmit}>
             <div className={classes.formInput}>
-              <label htmlFor="question1">
+              <label htmlFor="question1" className={classes.questionLabel}>
                 Which sort searches in linear time?
               </label>
               <Field id="question1" name="question1" />
             </div>
             <div className={classes.formInput}>
-              <label htmlFor="lastName">
+              <label className={classes.questionLabel}>
                 How many iterations will it take to find 8 in
                 [1,2,3,4,5,6,7,8,9]
               </label>
@@ -36,13 +44,13 @@ export function Quiz() {
                 className={classes.radioGroup}
               >
                 <label>
-                  <Field type="radio" name="question2" value={1} />1
+                  <Field type="radio" name="question2" value="One" />1
                 </label>
                 <label>
-                  <Field type="radio" name="question2" value={2} />2
+                  <Field type="radio" name="question2" value="Two" />2
                 </label>
                 <label>
-                  <Field type="radio" name="question2" value={3} />3
+                  <Field type="radio" name="question2" value="Three" />3
                 </label>
               </div>
             </div>
